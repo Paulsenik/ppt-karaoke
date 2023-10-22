5e7047b47941dcef7c941e3bb5dfa390e7da3c7a
Paulsenik <mail@paulsenik.de>
2023-10-22 12:19:54 +0200

    added presentations can be saved

--- src/main/java/de/paulsenik/pptkaraoke/utils/Presentation.java
+++ src/main/java/de/paulsenik/pptkaraoke/utils/Presentation.java
@@ -2,12 +2,10 @@ package de.paulsenik.pptkaraoke.utils;
 
 import de.paulsenik.jpl.io.PFile;
 import de.paulsenik.jpl.io.PFolder;
-import org.json.JSONArray;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 import java.util.List;
+import org.json.JSONObject;
 
 public record Presentation(
     String name,
@@ -40,27 +38,11 @@ public record Presentation(
     return year;
   }
 
-  public static void main(String[] args) {
-
-    ArrayList<String> test = new ArrayList<>();
-    test.add("qa35uhz");
-    test.add("qa35uh275z");
-    test.add("2457qa35uhz");
-    test.add("qa324575uhz");
-    test.add("qa35uh3246z");
-    JSONArray a = new JSONArray();
-    Presentation p = new Presentation("asdf", "adf", 234, test, test, Language.ENGLISH);
-    Presentation p2 = new Presentation("fhatj", "2436afhze", 246, test, test, Language.ENGLISH);
-    a.put(p.getSerialized());
-    //a.put(p2.getSerialized());
-    System.out.println(a.toString(3));
-  }
-
   public JSONObject getSerialized() {
     JSONObject obj = new JSONObject();
     obj.put("name", name);
     obj.put("language", language);
-    obj.put("tags", new JSONArray(tags));
+    obj.put("tags", tags);
     obj.put("topics", topics);
     return obj;
   }
--- src/main/java/de/paulsenik/pptkaraoke/utils/PresentationManager.java
+++ src/main/java/de/paulsenik/pptkaraoke/utils/PresentationManager.java
@@ -3,46 +3,105 @@ package de.paulsenik.pptkaraoke.utils;
 import de.paulsenik.jpl.io.PFile;
 import de.paulsenik.jpl.io.PFolder;
 import de.paulsenik.jpl.utils.PSystem;
-import java.sql.Array;
-import org.json.JSONArray;
-import org.json.JSONException;
-import org.json.JSONObject;
-
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
 
 public class PresentationManager {
 
-  public List<Presentation> presentations = new ArrayList<>();
+  public static String PPT_SAVE_FILE = "karaoke.json";
+
+  private final List<Presentation> presentations = new ArrayList<>();
 
   private String presentationDir;
   private String folderName;
 
   public PresentationManager(String presentationDir) {
-    initPresentations(presentationDir);
+    this.presentationDir = presentationDir;
+    try {
+      initPresentations();
+    } catch (Exception e) {
+      System.err.println("Initialization Error! Config may not be initialized completely!");
+      e.printStackTrace();
+    }
   }
 
-  private void initPresentations(String presentationDir) {
-    for (String folderPath : PFolder.getSubFolders(presentationDir)) {
-      JSONArray storage = getPresentationInfo(presentationDir, PFolder.getName(folderPath));
+  public static void main(String[] args) {
+
+    ArrayList<String> test = new ArrayList<>();
+    test.add("qa35uhz");
+    test.add("qa35uh275z");
+    test.add("2457qa35uhz");
+    test.add("qa324575uhz");
+    test.add("qa35uh3246z");
+    Presentation p = new Presentation("asdf", "adf", 234, test, test, Language.ENGLISH);
+    Presentation p2 = new Presentation("fhatj", "2436afhze", 246, test, test, Language.ENGLISH);
+
+    PresentationManager m = new PresentationManager(".");
+    m.presentations.add(p);
+    m.presentations.add(p2);
+    m.savePresentationInfo();
+  }
+
+  private void initPresentations() {
+    String[] subFolders = PFolder.getSubFolders(presentationDir);
+    JSONArray rawData = getRawPresentationInfo(presentationDir);
+
+    if (subFolders == null || rawData == null) {
+      return;
+    }
+
+    Map<String, JSONObject> data = getPresentationInfo(rawData);
+
+    for (String folderPath : subFolders) {
       for (String filePath : PFolder.getFiles(folderPath, null)) {
         // TODO
       }
     }
   }
 
-  private JSONArray getPresentationInfo(String presentationDir, String folderName) {
-    PFile file = new PFile(presentationDir + PSystem.getFileSeparator() + folderName + ".json");
+  private Map<String, JSONObject> getPresentationInfo(JSONArray data) {
+    if (data == null || data.isEmpty()) {
+      return null;
+    }
+
+    Map<String, JSONObject> map = new HashMap<>();
+    for (Object obj : data) {
+      if (obj instanceof JSONObject) {
+        JSONObject jsonObj = (JSONObject) obj;
+        map.put(jsonObj.getString("name"), jsonObj);
+      }
+    }
+
+    return map;
+  }
+
+  /**
+   * Reads the configuration for the presentations
+   *
+   * @param presentationDir
+   * @return the JSON-data - null if no valid file can be found
+   */
+  private JSONArray getRawPresentationInfo(String presentationDir) {
+    PFile file = new PFile(presentationDir + PSystem.getFileSeparator() + PPT_SAVE_FILE);
     if (!file.exists()) {
       return null;
     }
 
+    String content = file.getFileAsString();
+    if (content == null || content.isBlank()) {
+      return null;
+    }
+
     try {
-      return new JSONArray(file.getFileAsString());
+      return new JSONArray(content);
     } catch (JSONException e) {
       e.printStackTrace();
+      System.err.println("Content:\n" + content);
       return null;
     }
   }
@@ -50,20 +109,24 @@ public class PresentationManager {
   /**
    * Saves all presentation-Infos
    *
-   * @param presentationDir
-   * @return
+   * @return true if successful
    */
-  private boolean savePresentationInfo(String presentationDir) {
+  private boolean savePresentationInfo() {
     JSONArray storage = new JSONArray();
 
     for (Presentation p : presentations) {
       storage.put(p.getSerialized());
     }
 
-    // TODO
-
+    PFile file = new PFile(presentationDir + PSystem.getFileSeparator() + PPT_SAVE_FILE);
+    try {
+      file.writeFile(storage.toString(2));
+      System.out.println(file.getAbsolutePath());
+    } catch (Exception e) {
+      e.printStackTrace();
+      return false;
+    }
     return true;
   }
-
 
 }
